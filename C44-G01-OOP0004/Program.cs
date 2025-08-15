e8f26d73da669dfa0969d9a70d7e04b0685ca184
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:18:04 +0300

    PART 1

--- C44-G01-OOP0004/Program.cs
+++ C44-G01-OOP0004/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+        #region Part 1 - Multiple Choice Questions Answers
+        /*
+        Answers:
+        1. b) To define a blueprint for a class
+        2. a) private
+        3. b) No
+        4. b) Yes, interfaces can inherit from multiple interfaces
+        5. None (correct syntax is using colon ':')
+        6. a) Yes (starting with C# 8.0)
+        7. b) No, all members are implicitly public
+        8. b) To provide a clear separation between interface and class members
+        9. b) No, interfaces cannot have constructors
+        10. c) By separating interface names with commas
+        */
+        #endregion
+
     }
 }
